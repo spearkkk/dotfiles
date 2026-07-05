66338e6bc6ef8ea4056d182bf1ec4eac7dfa637e
spearkkk <spearkkp@gmail.com>
2026-07-05 13:41:40 +0900

    feat(sketchybar): add helpers/colors.lua

---
+++ .config/sketchybar/helpers/colors.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+M.base00 = "0xFF0A1F2E"
+M.base01 = "0xFF142C3E"
+M.base02 = "0xFF1C3A50"
+M.base03 = "0xFF24425C"
+M.base04 = "0xFF4A6E86"
+M.base05 = "0xFFC6D8E4"
+M.base06 = "0xFFD6E2E8"
+M.base07 = "0xFFECF0F4"
+M.base08 = "0xFFC47A72"
+M.base09 = "0xFFC8945A"
+M.base0a = "0xFFC8AE6A"
+M.base0b = "0xFF68BE92"
+M.base0c = "0xFF50C4C0"
+M.base0d = "0xFF7896CC"
+M.base0e = "0xFF9A7EC8"
+M.base0f = "0xFF8C6040"
+M.base10 = "0xFF071420"
+M.base11 = "0xFFD08880"
+M.base12 = "0xFFD4A870"
+M.base13 = "0xFFD4BE82"
+M.base14 = "0xFF80CCAA"
+M.base15 = "0xFF68D4D0"
+M.base16 = "0xFF90AED8"
+M.base17 = "0xFFAE96D4"
+
+M.background     = M.base00
+M.background_alt = M.base01
+M.foreground     = M.base05
+M.accent         = M.base0d
+M.border         = M.base03
+M.bar_border     = M.base00
+
+return M
